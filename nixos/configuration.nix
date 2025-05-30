649be9bdfecc9d63ee9efb22a454f44514078b89
Griffeth Barker <119845118+griffeth-barker@users.noreply.github.com>
2025-05-30 14:08:32 -0700

    fix vscode extensions

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -16,10 +12,6 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -43,7 +35,6 @@
   };
 
   # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
@@ -67,16 +58,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.griff = {
@@ -85,7 +67,6 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kdePackages.kate
-    #  thunderbird
     ];
   };
 
@@ -99,27 +80,26 @@
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile.
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  neovim
-  curl
-  wget
-  git
-  ghostty
-  discord
-  signal-desktop
-  spotify
-  bitwarden-desktop
-  powershell
-  oh-my-posh
-  neofetch
-  virtualbox
-  docker
-  docker-compose
-  obsidian
-  vscode
-  (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
+    neovim
+    curl
+    wget
+    git
+    ghostty
+    discord
+    signal-desktop
+    spotify
+    bitwarden-desktop
+    powershell
+    oh-my-posh
+    neofetch
+    virtualbox
+    docker
+    docker-compose
+    obsidian
+
+    (pkgs.vscode-with-extensions.override {
+      vscodeExtensions = with pkgs.vscode-extensions; [
         ms-vscode.powershell
         gruntfuggly.todo-tree
         github.github-vscode-theme
@@ -130,31 +110,12 @@
     })
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.allowedTCPPorts = [22];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  # This value determines the NixOS release
+  system.stateVersion = "25.05";
 }
