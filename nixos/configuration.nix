6bacc2f8337724221f8bd60a3066b18c5e85ba54
Griffeth Barker <119845118+griffeth-barker@users.noreply.github.com>
2025-07-07 21:19:08 -0700

    remove redundant configurations

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,3 +1,7 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
@@ -12,6 +16,10 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -35,6 +43,7 @@
   };
 
   # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
@@ -48,7 +57,7 @@
   };
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # services.printing.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
@@ -58,7 +67,16 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.griff = {
@@ -66,27 +84,64 @@
     description = "griff";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      kdePackages.kate
+      # kdePackages.kate
     ];
+    shell = pkgs.powershell;
   };
 
   # Install firefox.
   programs.firefox = {
     enable = true;
     policies = {
+      SkipTermsOfUse = true;
+      ShowHomeButton = false;
+      PasswordManagerEnabled = false;
+      OfferToSaveLoginsDefault = false;
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      ExtensionUpdat = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = false;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisplayBookmarksToolbar = "newtab";
+      DisplayMenuBar = "default-off";
+      SearchBar = "unified";
       ExtensionSettings = {
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4525374/bitwarden_password_manager-2025.6.1.xpi";
           installation_mode = "normal_installed";
+          default_area = "navbar";
         };
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4492375/ublock_origin-1.64.0.xpi";
           installation_mode = "normal_installed";
+          default_area = "navbar";
         };
         "addon@darkreader.org" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4488139/darkreader-4.9.106.xpi";
           installation_mode = "normal_installed";
+          default_area = "navbar";
         };
+      };
+      Preferences = {
+        "browser.formfill.enable" = false;
+        "browser.search.suggest.enabled" = false;
+        "browser.search.suggest.enabled.private" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.ml.chat.sidebar" = false;
+        "sidebar.expandOnHover" = true;
+        "sidebar.animation" = false;
+        "sidebar.verticalTabs" = true;
+        "sidebar.visibility" = "expand-on-hover";
       };
     };
   };
@@ -99,26 +154,24 @@
 
   # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
-    neovim
-    curl
-    wget
-    git
-    ghostty
-    discord
-    signal-desktop
-    telegram-desktop
-    spotify
-    bitwarden-desktop
-    powershell
-    oh-my-posh
-    neofetch
-    virtualbox
-    docker
-    docker-compose
-    obsidian
-
-    (pkgs.vscode-with-extensions.override {
-      vscodeExtensions = with pkgs.vscode-extensions; [
+  neovim
+  curl
+  wget
+  git
+  ghostty
+  discord
+  signal-desktop
+  spotify
+  powershell
+  oh-my-posh
+  neofetch
+  virtualbox
+  docker
+  docker-compose
+  obsidian
+  vscode
+  (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
         ms-vscode.powershell
         gruntfuggly.todo-tree
         github.github-vscode-theme
@@ -129,12 +182,38 @@
     })
   ];
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "python3.12-youtube-dl-2021.12.17"
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
+  # services.openssh.enable = true;
+  services.openssh = {  
+   enable = true;  
+   ports = [22];
+  };
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [22];
+  #networking.firewall.allowedTCPPorts = [22];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
 
-  # This value determines the NixOS release
-  system.stateVersion = "25.05";
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
 }
