efefc2474f83104f7618a82977932e7c281f578d
Griff Barker <github@griff.systems>
2025-07-07 20:18:19 -0700

    add firefox extension declarations

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -71,7 +71,25 @@
   };
 
   # Install firefox.
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    policies = {
+      ExtensionSettings = {
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4525374/bitwarden_password_manager-2025.6.1.xpi";
+          installation_mode = "normal_installed";
+        };
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4492375/ublock_origin-1.64.0.xpi";
+          installation_mode = "normal_installed";
+        };
+        "addon@darkreader.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4488139/darkreader-4.9.106.xpi";
+          installation_mode = "normal_installed";
+        };
+      };
+    };
+  };
 
   # Install steam.
   programs.steam.enable = true;
